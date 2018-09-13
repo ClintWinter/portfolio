5c548ac8501654eff9def8957d19796f1f3a690c
ClintWinter <ClintWinter@users.noreply.github.com>
2018-09-13 12:00:37 -0400

    color change, summary progress

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -6,6 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Clint Winter | Portfolio</title>
 
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
     <link rel="stylesheet" href="/css/app.css">
 </head>
 <body>
@@ -17,5 +18,6 @@
     </main>
 
     <script src="/js/app.js"></script>
+    
 </body>
 </html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,6 +1,31 @@
 @extends('layouts.master')
 @section('content')
 
-<h1 class="display-1">Hello World!</h1>
+<div class="row nospacing">
+
+    <div class="col-12 mb-5 text-center">
+        <img src="/images/linkedin.jpg" class="profile-img img-thumbnail">
+        <h1 class="display-1 text-center">Clint Winter</h1>
+        <div class="sm-links d-flex justify-content-center">
+            <a class="px-2" target="_blank" href="https://github.com/cwinterweb"><i class="fab fa-github-square"></i></a>
+            <a class="px-2" target="_blank" href="https://linkedin.com/in/clint-winter"><i class="fab fa-linkedin"></i></a>
+            {{-- <a href="https://github.com/cwinterweb"><i class="fab fa-github"></i></a>
+            <a href="https://github.com/cwinterweb"><i class="fab fa-github"></i></a> --}}
+        </div>
+        <hr class="bg-dark">
+    </div>
+
+    <div class="col-12 d-flex justify-content-between flex-nowrap">
+        <div class="flex-fill w-50 content-box mx-3 p-3">
+            <p>Greetings,</p>
+            <p>My name is Clint Winter, and I'm currently a full-stack web developer for <a target="_blank" href="https://cwrdistribution.com">CWR Wholesale Distribution</a>. I'm motivated to master the technologies and languages I know and continue adding new ones to my toolbelt. I've begun to add my projects and notes to my <a target="_blank" href="https://github.com/cwinterweb">GitHub</a> account.</p>
+            <p>Thanks for checking out my work, feel free to email me at <a href="mailto:cwinter.web@gmail.com">cwinter.web@gmail.com</a>!</p>
+        </div>
+        <div class="flex-fill w-50 content-box mx-3 p-3">
+            <p>Here is a list of my projects:</p>
+        </div>
+    </div>
+
+</div>
 
 @endsection
