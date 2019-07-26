214b5a029d36c6af84aed5f9eec81269e6721b2f
Clint Winter <clint.winter@gmail.com>
2019-07-26 14:28:31 -0400

    new js build

--- public/js/app.js
+++ public/js/app.js
@@ -732,7 +732,7 @@ module.exports = Cancel;
 /***/ (function(module, exports, __webpack_require__) {
 
 __webpack_require__(8);
-module.exports = __webpack_require__(30);
+module.exports = __webpack_require__(29);
 
 
 /***/ }),
@@ -742,7 +742,7 @@ module.exports = __webpack_require__(30);
 __webpack_require__(9);
 
 // modules
-__webpack_require__(29)();
+// require('./modules/startpage')();
 
 /***/ }),
 /* 9 */
@@ -752,13 +752,13 @@ window.axios = __webpack_require__(10);
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
-var token = document.head.querySelector('meta[name="csrf-token"]');
+// let token = document.head.querySelector('meta[name="csrf-token"]');
 
-if (token) {
-    window.axios.defaults.headers.common['X-CSRF-TOKEN'] = token.content;
-} else {
-    console.error('CSRF token not found: https://laravel.com/docs/csrf#csrf-x-csrf-token');
-}
+// if (token) {
+//     window.axios.defaults.headers.common['X-CSRF-TOKEN'] = token.content;
+// } else {
+//     console.error('CSRF token not found: https://laravel.com/docs/csrf#csrf-x-csrf-token');
+// }
 
 /***/ }),
 /* 10 */
@@ -1787,139 +1787,6 @@ module.exports = function spread(callback) {
 
 /***/ }),
 /* 29 */
-/***/ (function(module, exports) {
-
-module.exports = function () {
-
-    function init() {
-        var sections = [{
-            "name": "Google",
-            "links": [{
-                "title": "Maps",
-                "link": "https://maps.google.com",
-                "icon": "fas fa-map-pin"
-            }, {
-                "title": "Gmail",
-                "link": "https://mail.google.com",
-                "icon": "fas fa-envelope-square"
-            }, {
-                "title": "Docs",
-                "link": "https://docs.google.com",
-                "icon": "fas fa-file"
-            }, {
-                "title": "Drive",
-                "link": "https://drive.google.com",
-                "icon": "fab fa-google-drive"
-            }]
-        }, {
-            "name": "Entertainment",
-            "links": [{
-                "title": "Youtube",
-                "link": "https://youtube.com",
-                "icon": "fab fa-youtube"
-            }, {
-                "title": "Twitch",
-                "link": "https://twitch.tv",
-                "icon": "fab fa-twitch"
-            }]
-        }, {
-            "name": "Social",
-            "links": [{
-                "title": "LinkedIn",
-                "link": "https://linkedin.com",
-                "icon": "fab fa-linkedin"
-            }, {
-                "title": "Reddit",
-                "link": "https://reddit.com",
-                "icon": "fab fa-reddit"
-            }]
-        }, {
-            "name": "Finances",
-            "links": [{
-                "title": "TD Bank",
-                "link": "https://onlinebanking.tdbank.com/#/authentication/login",
-                "icon": "fas fa-money-check-alt"
-            }, {
-                "title": "Retirement",
-                "link": "https://www.go-retire.com/login",
-                "icon": "fas fa-piggy-bank"
-            }, {
-                "title": "Vanguard",
-                "link": "https://investor.vanguard.com/my-account/log-on",
-                "icon": "fas fa-chart-line"
-            }, {
-                "title": "Credit Card",
-                "link": "https://www.tdcardservices.com/CCServicing/Login.do?promoCode=default",
-                "icon": "fas fa-credit-card"
-            }]
-        }, {
-            "name": "Development",
-            "links": [{
-                "title": "Github",
-                "link": "https://github.com",
-                "icon": "fab fa-github"
-            }, {
-                "title": "Laracasts",
-                "link": "https://laracasts.com",
-                "icon": "fas fa-space-shuttle"
-            },
-            // {
-            //     "title": "Code Wars",
-            //     "link": "https://codewars.com/dashboard",
-            //     "icon": "fas fa-code"
-            // },
-            {
-                "title": "Digital Ocean",
-                "link": "https://cloud.digitalocean.com/projects",
-                "icon": "fab fa-digital-ocean"
-            }]
-        }];
-
-        var $section = $('section.links');
-
-        sections.forEach(function (section, index) {
-
-            var $card = $('<section></section>');
-            $card.addClass('card m-2 m-lg-3 py-3 px-5 flex-grow-1');
-
-            var $title = $('<h2></h2>');
-            $title.text(section.name);
-
-            var $ul = $('<ul></ul>');
-
-            $card.prepend($title, $ul);
-
-            // let card = `<section class="card"><h2>${section.name}</h2><ul>`;
-            section.links.forEach(function (link, index) {
-                // card += `<li><a href="${link.link}"><i class="${link.icon}"></i> <span>${link.title}</span></a></li>`;
-                var $li = $('<li></li>');
-
-                var $a = $('<a></a>');
-                $a.prop('href', link.link);
-
-                var $i = $('<i></i>');
-                $i.addClass(link.icon);
-
-                var $span = $('<span></span>');
-                $span.text(link.title);
-
-                $a.prepend($i, $span);
-                $li.prepend($a);
-                $ul.append($li);
-            });
-            // card += `</ul></section>`;
-
-            $section.append($card);
-        });
-    }
-
-    $(function () {
-        init();
-    });
-};
-
-/***/ }),
-/* 30 */
 /***/ (function(module, exports) {
 
 // removed by extract-text-webpack-plugin
