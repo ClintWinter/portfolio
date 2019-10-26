e1125f492ce8661f0a9b32e5644c7e8aefae01b2
Clint Winter <clint.winter@gmail.com>
2019-10-26 10:57:52 -0400

    library style tweaks

--- resources/js/components/FrontEnd/ResourceItem.vue
+++ resources/js/components/FrontEnd/ResourceItem.vue
@@ -1,26 +1,21 @@
 <template>
     <div class="max-w-full sm:max-w-1/2 lg:max-w-1/3 xl:max-w-1/4 mb-4 px-2">
-        <div class="h-full px-4 py-2 border border-gray-200 bg-gray-100">
-            <div class="flex items-end mb-4">
+        <div class="h-full px-4 py-2 border border-gray-200 bg-gray-100 flex flex-col justify-between">
+            <div class="flex mb-4">
                 <h3 class="mr-2 flex-grow" :title="resource.url">
                     <a
                         :href="resource.url"
                         target="_blank"
                         rel="noreferrer noopener"
                     >
-                        <span class="text-xl text-blue-500 hover:underline font-bold">{{ resource.name }}</span>
+                        <span class="leading-none text-xl text-blue-500 hover:underline font-bold">{{ resource.name }}</span>
                     </a><br>
                     <!-- <small class="text-gray-700">{{ resource.url }}</small> -->
                 </h3>
-                <p>{{ resource.resource_type }}</p>
+                <p class="leading-tight">{{ resource.resource_type }}</p>
             </div>
             <div class="flex flex-wrap justify-between">
                 <p class="text-gray-700 text-xs">{{ languageList }}</p>
-                <!-- <div
-                    class="px-2 py-1 text-pink-700 font-bold text-sm"
-                    v-for="language in resource.languages"
-                    :key="language.id"
-                >{{ language.name }}</div> -->
             </div>
         </div>
     </div>
