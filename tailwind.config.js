9c461df30a3ffebedc4e27da8be01da46a667385
ClintWinter <cwinter.web@gmail.com>
2022-01-26 12:10:18 -0500

    fix + update tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,56 +1,56 @@
 module.exports = {
-    purge: [
+    content: [
+        './storage/framework/views/*.php',
         './resources/**/*.blade.php',
         './resources/**/*.js',
         './resources/**/*.vue',
     ],
     theme: {
-      fontFamily: {
-        display: ['JetBrains Mono', 'Segoe UI', 'Helvetica Neue', 'Arial', 'sans-serif'],
-        body: ['Inter', 'Segoe UI', 'Helvetica Neue', 'Arial', 'sans-serif'],
-      },
-      minWidth: {
-        '0': '0',
-        '1/4': '25%',
-        '1/2': '50%',
-        '3/4': '75%',
-        '16': '4rem',
-        '32': '8rem',
-        '48': '12rem',
-        '64': '16rem',
-        '128': '32rem',
-        'full': '100%',
-      },
-      colors: {
-        'black': '#000000',
-        'darkbg': '#1C1E26',
-        'dark': '#8490A5',
-        'regular': '#CCCCCC',
-        'purple': '#B877DB',
-        'teal': '#25B0BC',
-        'yellow': '#FAB795',
-        'orange': '#F09483',
-        'red': '#E95678',
-      },
-      extend: {
-        maxHeight: {
-          '128': '32rem',
-          '60vh': '60vh',
+        fontFamily: {
+            display: ['JetBrains Mono', 'Segoe UI', 'Helvetica Neue', 'Arial', 'sans-serif'],
+            body: ['Inter', 'Segoe UI', 'Helvetica Neue', 'Arial', 'sans-serif'],
         },
-        height: {
-          '128': '32rem',
-          '256': '64rem',
+        colors: {
+            'black': '#000000',
+            'darkbg': '#1C1E26',
+            'dark': '#8490A5',
+            'regular': '#CCCCCC',
+            'purple': '#B877DB',
+            'teal': '#25B0BC',
+            'yellow': '#FAB795',
+            'orange': '#F09483',
+            'red': '#E95678',
         },
-        maxWidth: {
-          '128': '32rem',
-          '1/2': '50%',
-          '1/3': '33.333333%',
-          '1/4': '25%',
-          '1/5': '20%',
-          '1/6': '16.666667%',
-        },
-      }
+        extend: {
+            maxHeight: {
+                '128': '32rem',
+                '60vh': '60vh',
+            },
+            height: {
+                '128': '32rem',
+                '256': '64rem',
+            },
+            minWidth: {
+                '0': '0',
+                '1/4': '25%',
+                '1/2': '50%',
+                '3/4': '75%',
+                '16': '4rem',
+                '32': '8rem',
+                '48': '12rem',
+                '64': '16rem',
+                '128': '32rem',
+                'full': '100%',
+            },
+            maxWidth: {
+                '128': '32rem',
+                '1/2': '50%',
+                '1/3': '33.333333%',
+                '1/4': '25%',
+                '1/5': '20%',
+                '1/6': '16.666667%',
+            },
+        }
     },
-    variants: {},
-    plugins: []
-  }
+    plugins: [],
+}
--- webpack.mix.js
+++ webpack.mix.js
@@ -1,19 +1,19 @@
 const mix = require('laravel-mix');
 
-// require('laravel-mix-tailwind');
-
-mix.js('resources/js/app.js', 'public/js')
-   .js('resources/js/admin.js', 'public/js')
-   .js('resources/js/blog.js', 'public/js')
-   .js('resources/js/helper.js', 'public/js')
-   .js('resources/js/library.js', 'public/js')
-   .postCss('resources/css/app.css', 'public/css', [
+mix.postCss('resources/css/app.css', 'public/css', [
         require('postcss-import'),
+        require('tailwindcss/nesting'),
         require('tailwindcss'),
-        require('postcss-nested'),
    ]);
-//    .sass('resources/sass/app.scss', 'public/css');
 
 if (mix.inProduction()) {
     mix.version();
 }
+
+const webpackConfig = {
+    stats: {
+        children: true,
+    },
+};
+
+mix.webpackConfig(webpackConfig);
